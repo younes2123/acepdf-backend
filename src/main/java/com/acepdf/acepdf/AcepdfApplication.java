538d554a1e2472e3cddd267403a9319b7737dbdf
younes2123 <younestofo@gmail.com>
2025-09-25 17:38:12 +0100

    project starter

---
+++ src/main/java/com/acepdf/acepdf/AcepdfApplication.java
@@ -0,0 +1,13 @@
+package com.acepdf.acepdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcepdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcepdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acepdf/acepdf/AcepdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acepdf.acepdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcepdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
